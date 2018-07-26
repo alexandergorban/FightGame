85097c98294c03581ac40f1dbe1660954a21d207
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-26 18:14:47 +0300

    Added fight()

--- src/public/fight.js
+++ src/public/fight.js
@@ -1,1 +1,28 @@
 // create async function fight
+export async function fight(fighter, improvedFighter, ...points) {
+  console.log(`Fighter: ${fighter.name}`);
+  console.log(`ImprovedFighter: ${improvedFighter.name}`);
+  console.log('Start fight()');
+
+  for (let i = 0; i < points.length; i++){
+    console.log(`Point: ${i} - ${points[i]}`);
+
+    if (i%2 === 0){
+      fighter.hit(improvedFighter, points[i]);
+
+      if (improvedFighter.health <= 0){
+        console.log(`ImprovedFighter ${improvedFighter.name} knockout() with health ${improvedFighter.health}`);
+        await improvedFighter.knockout();
+        break;
+      }
+    } else {
+      improvedFighter.doubleHit(fighter, points[i]);
+
+      if (fighter.health <= 0){
+        console.log(`ImprovedFighter ${fighter.name} knockout() with health ${fighter.health}`);
+        await fighter.knockout();
+        break;
+      }
+    }
+  }
+}
